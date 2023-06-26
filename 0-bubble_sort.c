9cdda6c9e449ffb49d81c120ca8d600ddf17af87
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-26 19:06:25 +0000

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that sorts an array using bubble sort algorithm
+ * @array - the array to be sorted
+ * @size - the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int tmp, no_of_swap;
+
+	while (1)
+	{
+		no_of_swap = 0;
+		for (i = 0; i < (size - 1); ++i)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				++no_of_swap;
+				print_array(array, size);
+			}
+		}
+		if (no_of_swap == 0)
+			return;
+	}	
+}
